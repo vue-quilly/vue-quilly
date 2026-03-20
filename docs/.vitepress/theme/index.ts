8a1d60a3c2ee4e9ba8c159f1ced3557909614ae1
Oleksandr Shevchuk <34628096+alekswebnet@users.noreply.github.com>
2026-03-20 00:03:21 +0000

    Update deps, replace docs app

--- docs/.vitepress/theme/index.ts
+++
@@ -1,9 +0,0 @@
-import Layout from './Layout.vue'
-import Theme from 'vitepress/theme'
-import type { Theme as VitePressTheme } from 'vitepress'
-import './style.css'
-
-export default {
-  extends: Theme,
-  Layout
-} satisfies VitePressTheme
--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  ssr: true
+  ssr: true,
+  vite: {
+    optimizeDeps: {
+      include: [
+        'vue-quilly',
+        'quill/core',
+        'katex',
+        'quill',
+      ]
+    }
+  }
 })
